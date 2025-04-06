a127af91679eafb6ec930f9f1e83cfab9b0f5fa3
lchang4351 <lchang4351@gmail.com>
2025-04-06 17:52:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ChangYongChiangLionel-Capstone/'
+  base: '/ChangYongChiangLionel-Capstone/',
+  build: {
+    chunkSizeWarningLimit: 1600
+  }
 })
